de3031f417cc409785cc0f1f5197aa33d159498a
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-10-29 15:48:54 -0300

    Requisito 4 completo

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h2>Conteúdo aleatório sobre os planetas</h2>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
